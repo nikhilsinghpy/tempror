ccfaea7d99dd36904aa949749ef6ef9fc9259c7b
nikhilsinghpy <nikhilsingh1139@gmail.com>
2025-12-05 22:32:17 +0530

    some

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const BASE_URL = "http://localhost:8080/api/v1";
-// const BASE_URL = "https://api-belleza.onrender.com/api/v1";
-const BASE_URL = "http://34.131.223.22/api/v1";
+const BASE_URL = "https://api-belleza.onrender.com/api/v1";
+// const BASE_URL = "http://34.131.223.22/api/v1";
 
 /**
  * Create a reusable Axios instance
