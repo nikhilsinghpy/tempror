cce4fa853ed61736b9d5a9b02566f857f1452d98
nikhilsinghpy <nikhilsingh1139@gmail.com>
2025-12-14 15:44:21 +0530

    some

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -3,10 +3,10 @@ import axios from "axios";
 // DEVELOPMENT SERVER URL
 // const BASE_URL = "http://localhost:4040/api/v1";
 // const BASE_URL = "http://34.131.223.22/api/v1";
-const BASE_URL = "http://192.168.0.5:4040/api/v1";
+// const BASE_URL = "http://192.168.0.5:4040/api/v1";
 
 // TESTING SERVER URL
-// const BASE_URL = "https://api-belleza.onrender.com/api/v1";
+const BASE_URL = "https://api-belleza.onrender.com/api/v1";
 
 // PRODUCTION SERVER URL
 // const BASE_URL = "https://api.bellezarohini.com/api/v1";
