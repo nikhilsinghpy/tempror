6945cbdabf9e35f52f17423da49bf5112d8ba239
nikhilsinghpy <nikhisingh1139@gmail.com>
2026-02-02 23:47:38 +0530

    some

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -5,11 +5,12 @@ import axios from "axios";
 // const BASE_URL = "http://34.131.223.22/api/v1";
 // const BASE_URL = "http://192.168.0.5:8080/api/v1";
 
-// TESTING SERVER URL
-// const BASE_URL = "https://api-belleza.onrender.com/api/v1";
 
-// PRODUCTION SERVER URL
-const BASE_URL = "https://api-belleza-jvlz.onrender.com/api/v1";
+// Testing SERVER URL
+// const BASE_URL = "https://api-belleza-jvlz.onrender.com/api/v1";
+
+//production url 
+const BASE_URL = "https://api.bellezarohini.com/api/v1";
 
 /**
  * Create a reusable Axios instance
